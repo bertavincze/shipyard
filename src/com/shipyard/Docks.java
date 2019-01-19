67554731f7c45fdddab41eb4cb65288e54857ac9
Berta Vincze <berta.vincze@gmail.com>
2019-01-19 22:56:14 +0100

    Remove hardcoded bits from Docks class

--- src/com/shipyard/Docks.java
+++ src/com/shipyard/Docks.java
@@ -2,20 +2,32 @@ package com.shipyard;
 
 public class Docks {
 
+    private String name;
+    private int size;
     private Ship[] ships;
 
-    public Docks() {
-        this.ships = createShips();
+    public Docks(String name, int size) {
+        this.name = name;
+        this.size = size; 
+        this.ships = createShips(size);
     }
 
-    public Ship[] createShips() {
-        Ship[] ships = new Ship[10];
+    public Ship[] createShips(int num) {
+        Ship[] ships = new Ship[num];
         for (int i = 0; i < ships.length; i++) {
             ships[i] = new Ship();
         }
         return ships; 
     }
 
+    public String getName() {
+        return this.name;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+    
     public Ship[] getShips() {
         return this.ships;
     }
--- src/com/shipyard/Main.java
+++ src/com/shipyard/Main.java
@@ -6,9 +6,11 @@ public class Main {
 
     public static void main(String[] args) {
             System.out.println("TESTING");
+            System.out.println();
             
-            Docks docks = new Docks();
-            docks.createShips();
+            Docks docks = new Docks("Test Bay", 20);
+            System.out.println(docks.getName());
+            System.out.println();
             for (Ship ship : docks.getShips()) {
                 System.out.println(ship.toString()); 
             }
